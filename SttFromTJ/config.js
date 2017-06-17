2170afdc3bc145406744c9c8c07371f2b0812e34
Markus van Kempen <markusvankempen43@gmail.com>
2017-06-17 13:57:05 -0400

    Create config.js

---
+++ SttFromTJ/config.js
@@ -0,0 +1,21 @@
+/*
+User-specific configuration
+    ** IMPORTANT NOTE ********************
+    * Please ensure you do not interchange your username and password.
+    * Hint: Your username is the lengthy value ~ 36 digits including a hyphen
+    * Hint: Your password is the smaller value ~ 12 characters
+*/ 
+
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+    password: 'YOURSPWD',
+    username: 'fbc84635-9f24-48e0-902c-5cf17664ca7b'
+};
+exports.credentials.text_to_speech = {
+    password: 'YOURSPWD',
+    username: 'd0868abe-bde9-4458-a2e6-10fa33f89c96'
+};
